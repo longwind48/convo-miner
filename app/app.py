205530c4ec6054127305077dcafee237d283d5f0
I353305 <lijie.quan@sap.com>
2019-05-06 22:58:49 +0800

    move used data to app folder

--- app/app.py
+++ app/app.py
@@ -21,9 +21,9 @@ def home():
 def result_frame_seq():
     global pride_prejudice_link
     page_label = iob_to_html_tags(pride_prejudice_link,
-                                  "../data/parsed-n-labeled-data/iob-labeled-sent-final-060519-v2.csv")
+                                  "data/iob-labeled-sent-final-060519-v2.csv")
     page_pred = iob_to_html_tags(pride_prejudice_link,
-                                 "../data/predicted/seq_label_ner-v4-pred.csv")
+                                 "data/seq_label_ner-v4-pred.csv")
 
     return render_template('result_frame.html', webpage1=page_label, webpage2=page_pred)
 
@@ -32,9 +32,9 @@ def result_frame_seq():
 def result_frame_heuristic():
     global pride_prejudice_link
     page_label = iob_to_html_tags(pride_prejudice_link,
-                                  "../data/parsed-n-labeled-data/iob-labeled-sent-final-060519-v2.csv")
+                                  "data/iob-labeled-sent-final-060519-v2.csv")
     page_pred = iob_to_html_tags(pride_prejudice_link,
-                                 "../data/predicted/heuristic_pred_v2.csv")
+                                 "data/heuristic_pred_v2.csv")
 
     return render_template('result_frame.html', webpage1=page_label, webpage2=page_pred)
 
